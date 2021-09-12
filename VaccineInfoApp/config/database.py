60ab5c5f69f1884bf544497c54861a0c680f0208
youMs <urmad129@gmail.com>
2021-09-12 21:21:52 +0530

    authentication code

--- VaccineInfoApp/config/database.py
+++ VaccineInfoApp/config/database.py
@@ -1,16 +1,33 @@
-from importlib.metadata import metadata
-from venv import create
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from importlib.metadata import metadata
+# from venv import create
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
-from mysql.connector import Error, MySQLConnection
+# from mysql.connector import Error, MySQLConnection
 
-engine = create_engine("mysql+pymysql://root@127.0.0.1:3306/citizenschema")
+# engine = create_engine("mysql+pymysql://root@127.0.0.1:3306/citizenschema")
 
-connObject = engine.connect()
+# connObject = engine.connect()
     
 
+
+fake_users_db = {
+    "johndoe": {
+        "username": "umeshm",
+        "full_name": "Umesh MMMM",
+        "email": "umeshm@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "chinmay",
+        "full_name": "chinmay MMM",
+        "email": "chinmay@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root@localhost:3306/citizenschema"
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- VaccineInfoApp/index.py
+++ VaccineInfoApp/index.py
@@ -1,11 +1,21 @@
 ################## imports ##########################################
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
-# from .config.database import connection
+from .config.database import fake_users_db
 # from routes.index import user
 import mysql.connector
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 ################# models ###########################################
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+class UserInDB(User):
+    hashed_password: str
 
 class VaccineInfo(BaseModel):
     doseDate : str
@@ -36,6 +46,7 @@ class CitizenVaccineDetails(BaseModel):
 
 #################### objects ########################################
 app =  FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 #############################################################################################
 #                                    REQUIREMENT                                            #
 # create entry in tbcitizen [citizenId, firstName, lastName, PhoneNumber, emailId]          #
@@ -54,7 +65,7 @@ app =  FastAPI()
 #############################################################################################
 ################### routing methods ###################################
 @app.post('/createTbCitizenEntry')
-def createNewCitizenAndVaccineInfo(citizenInfoRequest : CitizenVaccineDetails):
+def createNewCitizenAndVaccineInfo(citizenInfoRequest : CitizenVaccineDetails, token: str = Depends(oauth2_scheme)):
     try:
         connection = mysql.connector.connect(host='127.0.0.1',
                                              database='citizenschema',
@@ -118,6 +129,53 @@ def retrieveDbRecordsById(id : int):
 
     cursor.close()
     connection.close()
+
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
 ################################################################
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
 ############################################################
